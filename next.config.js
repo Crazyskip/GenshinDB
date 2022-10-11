6783632ba72affc74a976767e8baaf120a487f5e
crazyskip <crazyskip73@gmail.com>
2022-10-11 10:33:16 +1000

    Update to 3.2 Content

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
-const withPWA = require("next-pwa");
-
 module.exports = {
   reactStrictMode: true,
 };
-
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-  },
-});
--- pages/abyss.tsx
+++ pages/abyss.tsx
@@ -30,14 +30,14 @@ const Abyss: NextPage = () => {
         <div className="flex flex-wrap mt-4">
           <div className="w-full xl:w-1/2 p-2">
             <a
-              href="https://preview.redd.it/u75ri8d05w291.png?width=2981&format=png&auto=webp&s=3ec70f4ca2ff6ff07be7c3b4cb75f28b13142388"
+              href="https://preview.redd.it/r90wgypft3r91.png?width=3230&format=png&auto=webp&s=9f98668802f5bfe9f8ee29b38b2aea1412c50f9f"
               target="_blank"
               rel="noreferrer"
             >
               <Image
                 src="/assets/abyss/floor9.webp"
                 alt="abyss floor 9"
-                width={1920}
+                width={2198}
                 height={1080}
                 layout="responsive"
               />
@@ -45,14 +45,14 @@ const Abyss: NextPage = () => {
           </div>
           <div className="w-full xl:w-1/2 p-2">
             <a
-              href="https://preview.redd.it/tai5xbd05w291.png?width=2981&format=png&auto=webp&s=08d840d8e715bbf6390ea38cf2c2a12d78dd58ee"
+              href="https://preview.redd.it/aiet3brft3r91.png?width=3230&format=png&auto=webp&s=2412dc95cea85da671814526eadecab854c24f74"
               target="_blank"
               rel="noreferrer"
             >
               <Image
                 src="/assets/abyss/floor10.webp"
                 alt="abyss floor 10"
-                width={1920}
+                width={2198}
                 height={1080}
                 layout="responsive"
               />
@@ -61,14 +61,14 @@ const Abyss: NextPage = () => {
 
           <div className="w-full xl:w-1/2 p-2">
             <a
-              href="https://preview.redd.it/kkayrvb2o7c91.png?width=3230&format=png&auto=webp&s=8d2e7d688333f14eb933bba62791784879665118"
+              href="https://preview.redd.it/zf30zlqft3r91.png?width=3230&format=png&auto=webp&s=836fd6a52404c4cec96b3feadf70e563bbe441cf"
               target="_blank"
               rel="noreferrer"
             >
               <Image
                 src="/assets/abyss/floor11.webp"
                 alt="abyss floor 11"
-                width={1920}
+                width={2198}
                 height={1080}
                 layout="responsive"
               />
@@ -76,14 +76,14 @@ const Abyss: NextPage = () => {
           </div>
           <div className="w-full xl:w-1/2 p-2">
             <a
-              href="https://preview.redd.it/b3ko5erqp7c91.png?width=3230&format=png&auto=webp&s=ee66ee7985f154b8e0d69afe7279f5db43184d18"
+              href="https://preview.redd.it/myu5ulqft3r91.png?width=3229&format=png&auto=webp&s=185a574e7d4e39e628f34fc2612c34e378e1e2ca"
               target="_blank"
               rel="noreferrer"
             >
               <Image
                 src="/assets/abyss/floor12.webp"
                 alt="abyss floor 12"
-                width={1920}
+                width={2198}
                 height={1080}
                 layout="responsive"
               />
